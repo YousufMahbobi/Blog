b952d86aac47f3602d31b01a395c17a9fd9c216a
Sayed Yousuf Mahbobi <rayanguzman345@gmail.com>
2025-07-13 17:02:43 +0200

    Update functionality added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from data.json_file_handler import read_json, write_json
 app = Flask(__name__)
 
@@ -33,6 +33,32 @@ def delete(post_id):
             write_json(blog_posts)
     return redirect(url_for('index'))
 
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    edit_post_obj = None
+    blog_posts = read_json()
+    for post in blog_posts:
+        if post['id'] == post_id:
+            edit_post_obj = post
+            if request.method == 'POST':
+                author = request.form['author']
+                title = request.form['title']
+                content = request.form['content']
+                post['author'] = author
+                post['title'] = title
+                post['content'] = content
+                write_json(blog_posts)
+                return redirect(url_for('index'))
+            else:
+                return render_template('update.html', eidt_post_obj=edit_post_obj)
+    if edit_post_obj is None:
+        return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
